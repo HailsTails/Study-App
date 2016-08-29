107fe8d49b12fc181a3ce658bde0550fc9d2be7c
HailsTails <HelenCodeMonkey@gmail.com>
2016-08-29 20:05:29 +1000

    Login and test angular

--- public/myapp.js
+++ public/myapp.js
@@ -1,14 +1,42 @@
 var app = angular.module('studyApp', []);
 
 app.controller('MainCtrl', [
-'$scope',
-function($scope){
-  $scope.test = 'Hello world!';
+  '$scope',
+  function($scope){
+    $scope.test = 'Hello world!';
+
+    //Define initial tasks
+    $scope.tasks = [
+      {id: 1, title: 'assignment 1', due_date: 7, done: ' '},
+      {id: 2, title: 'Essay 4', due_date: 3, done: 'x'}
+    ];
+
+  //Add Tasks
+  $scope.addTask = function(){
+    if(!$scope.title || $scope.title === '') { return; }
+    $scope.tasks.push({
+      id: 3,
+      title: $scope.title,
+      due_date: $scope.due_date,
+      done: ' '});
+    $scope.title = '';
+    $scope.due_date = '';
+  }
+
+  //Tick off a task (change it to done)
+  $scope.tickOff = function(task){
+    if(task.done == ' '){
+        task.done = 'x';
+    }
+    else if(task.done == 'x'){
+        task.done = ' ';
+    }
+  }
 }]);
 
+//set CLient ID and google auth settings
 var _auth2;
 var client_id = "73713153185-ml2ui4qa12p4tpfgiuhgebpsi374gnqc.apps.googleusercontent.com";
-//set CLient ID and google auth settings
 var _onGoogleLoad = function(){
   gapi.load('auth2', function(){
     auth2 = gapi.auth2.init({
@@ -21,20 +49,17 @@ var _onGoogleLoad = function(){
 function onSignIn(googleUser) {
   var id_token = googleUser.getAuthResponse().id_token;
 
-  var verifier = require('google-id-token-verifier');
-
-  verifier.verify(id_token, clientId, function (err, tokenInfo) {
-    if (!err) {
-      // use tokenInfo in here.
-      var userID = tokenInfo['sub']
-      console.log(userID);
+  $.get(
+    "https:/www.googleapis.com/oauth2/v3/tokeninfo",
+    {id_token : id_token},
+    function(data) {
+      if(data['aud']==client_id){
+        console.log(data);
+      }
     }
-  });
+  );
 
-    //database login
-    //post here
-
-  }
+}
 
 //Sign out a user
 function signOut() {
